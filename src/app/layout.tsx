a90d3a95e5e82372bf6c6a1cd94c783e73ba3807
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-08-28 13:54:09 -0400

    Moved View Students and Leaderboards to Nav Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,17 +19,29 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="px-24 py-6">
-          <nav className="flex flex-row justify-between pb-6 font-bold text-xl">
-            <Link href="/" className="hover:text-blue-500">
-              Home
-            </Link>
-            <Link href="/import" className="hover:text-blue-500">
-              Import Data
-            </Link>
-          </nav>
+          <NavBar />
           {children}
         </main>
       </body>
     </html>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="flex flex-row justify-between pb-6 font-bold text-xl">
+      <NavButton href="/" label="Home" />
+      <NavButton href="/students" label="View Students" />
+      <NavButton href="/leaderboards" label="Leaderboards" />
+      <NavButton href="/import" label="Import Data" />
+    </nav>
+  );
+}
+
+function NavButton({ href, label }: { href: string; label: string }) {
+  return (
+    <Link href={href} className="hover:text-blue-500">
+      {label}
+    </Link>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import Link from "next/link";
 import PageHeader from "./(components)/PageHeader";
 
 export default async function HomePage() {
   return (
     <>
       <PageHeader title="Brainskills Dashboard" />
-      <div className="flex flex-col items-center gap-8">
-        <Link
-          href="/students"
-          className="px-5 py-2 text-lg font-semibold text-white bg-blue-500 rounded-md shadow-md hover:shadow-lg"
-        >
-          View Students
-        </Link>
-        <Link
-          href="/leaderboards"
-          className="px-5 py-2 text-lg font-semibold text-white bg-blue-500 rounded-md shadow-md hover:shadow-lg"
-        >
-          Leaderboards
-        </Link>
-      </div>
     </>
   );
 }
